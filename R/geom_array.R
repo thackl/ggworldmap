5b8a04341c128b57749df530f8130d757e49e57c
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2018-03-03 12:12:43 -0500

    ncol & nmax for geom_array to compress data: one array point represents multiple data points

--- R/geom_array.R
+++ R/geom_array.R
@@ -1,12 +1,12 @@
 #' @export
 geom_array <- function(mapping = NULL, data = NULL, stat = "array",
-    position = "identity", ..., nrow = 10, spread = c(x = 4,y = 4),
+    position = "identity", ..., nrow = 10, ncol = NULL, spread = c(x = 4,y = 4),
     hjust = 0.5, vjust = 0, show.legend = NA, inherit.aes = TRUE){
 
   layer(
     data = data, mapping = mapping, stat = stat, geom = GeomPoint,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(nrow = nrow, spread = spread, hjust = hjust, vjust = vjust, na.rm = FALSE, ...))
+    params = list(nrow = nrow, ncol = ncol, spread = spread, hjust = hjust, vjust = vjust, na.rm = FALSE, ...))
 }
 ## #' @export
 ## stat_array <- function(mapping = NULL, data = NULL, geom = "point",
@@ -21,32 +21,59 @@ geom_array <- function(mapping = NULL, data = NULL, stat = "array",
 #' @export
 StatArray <- ggproto(
   "StatArray", Stat,
-  compute_panel = function(data, scales, nrow = 10, spread = c(4,4),
+  compute_panel = function(data, scales, nrow = 10, ncol = NULL, spread = c(4,4),
       hjust = 0.5, vjust = 0.5){
-    data <- data %>%
-      dplyr::group_by(x, y) %>%
+
+    nmax <- dplyr::count(data, x, y) %>% pull(n) %>% max()
+
+    if(!is.null(ncol)){
+      nmax %||% stop('nmax required if ncol specified')
+      m <- ceiling(nmax / (ncol * nrow))
+      cat("geom_array: showing one array point per", m, "data points\n")
+    }
+
+    data <- dplyr::group_by(data, x, y) %>%
       dplyr::mutate( 
-        .x=stack_x(x, nrow, spread[1], hjust),
-        .y=stack_y(y, nrow, spread[length(spread)], vjust)
+        .x=stack_x(x, nrow = nrow, ncol = ncol, nmax = nmax, spread = spread[1], hjust = hjust),
+        .y=stack_y(y, nrow = nrow, ncol = ncol, nmax = nmax, spread = spread[length(spread)], vjust = vjust)
       ) %>% dplyr::ungroup() %>% # cannot modify grouping var
       dplyr::mutate(x = .x, .x = NULL, y = .y, .y = NULL)
-    print(data %>% as_tibble)
     data
   },
   required_aes = c("x","y")
 )
 
-stack_x <- function(x, nrow = 5, spread = 1, hjust = .5){
+stack_x <- function(x, nrow = 5, ncol = NULL, nmax = NULL, spread = 1, hjust = .5){
   n <- length(x)
-  ncol <- ceiling(n / nrow)
-  o <- (rep(seq_len(ncol), each=nrow)[seq_len(n)] - 1) * spread
+
+  if(is.null(ncol)){
+    ncol <- ceiling(n / nrow)
+    m <- 1 # default multiplicator
+  }else{
+    nmax %||% stop('nmax required if ncol specified')
+    # multiplicator: stack points if there are more than slots in array
+    m <- ceiling(nmax / (ncol * nrow))
+  }
+
+  # offset for each point
+  o <- (rep(seq_len(ncol), each=nrow * m)[seq_len(n)] - 1) * spread
   x + o - max(o) * hjust
 }
 
-
-stack_y <- function(y, nrow = 5, spread = 1, vjust = 0){
+stack_y <- function(y, nrow = 5, ncol = NULL, nmax = NULL, spread = 1, vjust = 0){
   n <- length(y)
-  ncol <- ceiling(n / nrow)
-  o <- (rep(seq_len(nrow), ncol)[seq_len(n)] -1) * spread
+  m <- 1 # default multiplicator
+
+  if(is.null(ncol)){
+    ncol <- ceiling(n / nrow)
+    m <- 1 # default multiplicator
+  }else{
+    nmax %||% stop('nmax required if ncol specified')
+    # multiplicator: stack points if there are more than slots in array
+    m <- ceiling(nmax / (ncol * nrow))
+  }
+
+  # offset for each point
+  o <- (rep(rep(seq_len(nrow), ncol), each = m)[seq_len(n)] -1) * spread
   y + o - max(o) * vjust
 }
