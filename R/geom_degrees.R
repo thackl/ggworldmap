9f25329db7b66a388c4dbfdbc2e357d3ac8658e8
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2018-03-03 23:41:13 -0500

    use different vjust/hjust for degree lat [center,outward] and long [outward,center]

--- R/geom_degrees.R
+++ R/geom_degrees.R
@@ -1,10 +1,10 @@
 #' @export
-geom_degree <- function(mapping = NULL, data = NULL, stat = "identity",
-    position = "identity", proj = NULL, long_0 = 0,
-    lat_min = -90, lat_max = 90, lat_by = 20, lat_n = NULL,
-    lat_at = c(long_min, long_max), lat_nudge = 5,
-    long_min = -180 + long_0, long_max = 180 + long_0, long_by = 30,
-    long_n = NULL, long_at = c(lat_min, lat_max), long_nudge = 10,
+geom_degree <- function( mapping = NULL, data = NULL, stat = "identity",
+    position = "identity", proj = NULL, long_0 = 0, lat_min = -90, lat_max = 90,
+    lat_by = 20, lat_n = NULL, lat_at = c(long_min, long_max), lat_nudge = 5,
+    lat_vjust = "center", lat_hjust = "outward", long_min = -180 + long_0,
+    long_max = 180 + long_0, long_by = 30, long_n = NULL, long_at = c(lat_min, lat_max),
+    long_nudge = 5, long_vjust = "outward", long_hjust = "center",
     proj_extra=NULL, ..., na.rm = FALSE, show.legend = NA, inherit.aes = FALSE){
 
   long_0 <- long_0 %||% 0
@@ -18,8 +18,19 @@ geom_degree <- function(mapping = NULL, data = NULL, stat = "identity",
     long_by = long_by, long_n = long_n, long_at = long_at, long_nudge = long_nudge,  
     proj_extra=proj_extra)
 
-  ggplot2::geom_text(mapping = mapping, data = data, stat = stat, position = position, ...,
-    na.rm = na.rm, show.legend = show.legend, inherit.aes = inherit.aes) 
+  # use different vjust/hjust for lat/long if that information is available
+  if("type" %in% names(data)){
+    data$hjust <- c(lat_hjust, long_hjust)[match(data$type, c("lat", "long"))]
+    data$vjust <- c(lat_vjust, long_vjust)[match(data$type, c("lat", "long"))]
+    mapping <- aes_intersect(mapping, aes_(hjust = ~hjust, vjust = ~vjust))
+    ggplot2::geom_text(mapping = mapping, data = data, stat = stat, position = position, ...,
+      na.rm = na.rm, show.legend = show.legend, inherit.aes = inherit.aes)
+  }else{
+    cat("Need column 'type' with values 'lat/long' in data to apply lat/long_vjust/hjust\n",
+        "Falling back to vjust/hjust for all labels.\n")
+    ggplot2::geom_text(mapping = mapping, data = data, stat = stat, position = position, ...,
+      na.rm = na.rm, show.legend = show.legend, inherit.aes = inherit.aes)
+  }
 }
 #' @export
 degrees <- function(proj = NULL, long_0 = 0, lat_min = -90, lat_max = 90,
@@ -51,7 +62,7 @@ degrees <- function(proj = NULL, long_0 = 0, lat_min = -90, lat_max = 90,
   degrees_long <- mutate(degrees_long, lat = ifelse(lat >0, lat + lat_nudge, lat - lat_nudge))
   degrees_lat <- mutate(degrees_lat, long = ifelse(long >0, long + long_nudge, long - long_nudge))
 
-  dplyr::bind_rows(degrees_lat, degrees_long)
+  dplyr::bind_rows(.id = "type", lat=degrees_lat, long=degrees_long)
 }
 
 wrap_dd <- function(r, shift=0){ # between -180 and 180
