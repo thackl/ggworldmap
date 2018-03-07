f78b16bd173e161a4cf2bd111145392ef25c64cc
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2018-03-07 11:25:18 -0500

    fixed some issues with geom_degree lat/long nudging. replaced lat/long_nudge by lat/long_nudge_x/y

--- R/geom_degrees.R
+++ R/geom_degrees.R
@@ -1,10 +1,11 @@
 #' @export
 geom_degree <- function(mapping = NULL, data = NULL, stat = "identity",
     position = "identity", proj = NULL, long_0 = 0, lat_min = -90, lat_max = 90,
-    lat_by = 20, lat_n = NULL, lat_at = c(long_min, long_max), lat_nudge = 5,
-    lat_vjust = "center", lat_hjust = "outward", long_min = -180 + long_0,
-    long_max = 180 + long_0, long_by = 30, long_n = NULL, long_at = c(lat_min, lat_max),
-    long_nudge = 5, long_vjust = "outward", long_hjust = "center",
+    lat_by = 20, lat_n = NULL, lat_at = c(long_min, long_max), lat_nudge_x = 5,
+    lat_nudge_y = 0, lat_vjust = "center", lat_hjust = "outward", 
+    long_min = -180 + long_0, long_max = 180 + long_0, long_by = 30, 
+    long_n = NULL, long_at = c(lat_min, lat_max), long_nudge_x = 0,
+    long_nudge_y =5, long_vjust = "outward", long_hjust = "center",
     proj_extra=NULL, ..., na.rm = FALSE, show.legend = NA, inherit.aes = FALSE){
 
   long_0 <- long_0 %||% 0
@@ -14,9 +15,9 @@ geom_degree <- function(mapping = NULL, data = NULL, stat = "identity",
 
   data <- data %||% degrees(proj = proj, long_0 = long_0, lat_min = lat_min,
     lat_max = lat_max, lat_by = lat_by, lat_n = lat_n, lat_at = lat_at,
-    lat_nudge = lat_nudge, long_min = long_min, long_max = long_max,
-    long_by = long_by, long_n = long_n, long_at = long_at, long_nudge = long_nudge,  
-    proj_extra=proj_extra)
+    lat_nudge_x = lat_nudge_x, lat_nudge_y = lat_nudge_y, long_min = long_min,
+    long_max = long_max, long_by = long_by, long_n = long_n, long_at = long_at, 
+    long_nudge_x = long_nudge_x, long_nudge_y = long_nudge_y, proj_extra=proj_extra)
 
   # use different vjust/hjust for lat/long if that information is available
   if("type" %in% names(data)){
@@ -35,8 +36,9 @@ geom_degree <- function(mapping = NULL, data = NULL, stat = "identity",
 #' @export
 degrees <- function(proj = NULL, long_0 = 0, lat_min = -90, lat_max = 90,
     lat_by = 20, lat_n = NULL, lat_at = c(long_min, long_max),
-    lat_nudge = 5, long_min = -180 + long_0, long_max = 180 + long_0, long_by = 30,
-    long_n = NULL, long_at = c(lat_min, lat_max), long_nudge = 10,
+    lat_nudge_x = 5, lat_nudge_y = 0, long_min = -180 + long_0,
+    long_max = 180 + long_0, long_by = 30, long_n = NULL,
+    long_at = c(lat_min, lat_max), long_nudge_x = 0, long_nudge_y = 10, 
     proj_extra=NULL){
 
   long_0 <- long_0 %||% 0
@@ -59,8 +61,14 @@ degrees <- function(proj = NULL, long_0 = 0, lat_min = -90, lat_max = 90,
     degrees_long %<>% project(proj, long_0, proj_extra=proj_extra)
   }
   
-  degrees_long <- mutate(degrees_long, lat = ifelse(lat >0, lat + lat_nudge, lat - lat_nudge))
-  degrees_lat <- mutate(degrees_lat, long = ifelse(long >0, long + long_nudge, long - long_nudge))
+  degrees_lat <- mutate(
+    degrees_lat,
+    lat = ifelse(lat >0, lat + lat_nudge_y, lat - lat_nudge_y),
+    long = ifelse(long >0, long + lat_nudge_x, long - lat_nudge_x))
+  degrees_long <- mutate(
+    degrees_long,
+    lat = ifelse(lat >0, lat + long_nudge_y, lat - long_nudge_y),
+    long = ifelse(long >0, long + long_nudge_x, long - long_nudge_x))
 
   dplyr::bind_rows(.id = "type", lat=degrees_lat, long=degrees_long)
 }
