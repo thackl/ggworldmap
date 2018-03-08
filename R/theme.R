7de81e404c3d3847b9f3813290ca1094cf122004
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2018-03-07 23:27:21 -0500

    removed panel.background border from theme

--- R/theme.R
+++ R/theme.R
@@ -3,6 +3,7 @@ theme_worldmap_light <- function(...){
   theme_light(base_size = 15) +
     theme(
       panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-      axis.text = element_blank(), axis.ticks = element_blank(), axis.title = element_blank()) +
+      axis.text = element_blank(), axis.ticks = element_blank(), axis.title = element_blank(),
+      panel.border = element_rect(color = NA)) +
     theme(...)
 }
