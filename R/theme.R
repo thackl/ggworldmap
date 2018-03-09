ab3956c36bf39103667c4e19ad338c4a17df47c6
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2018-03-09 15:20:44 -0500

    theme doc

--- R/theme.R
+++ R/theme.R
@@ -1,4 +1,19 @@
+#' Worldmap themes
+#'
+#' Themes based on ggplot2 themes that play well with ggworldmaps.
+#'
+#' @name theme_worldmap
+#' @details
+#' \describe{
+#'
+#' \item{`theme_gray`}{A theme based on ‘ggplot2::theme_light’, with light grey
+#' lines and axes, to direct more attention towards the data.}
+#'
+#' }
+NULL
+
 #' @export
+#' @rdname theme_worldmap
 theme_worldmap_light <- function(...){
   theme_light(base_size = 15) +
     theme(
